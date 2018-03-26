f8fd833da4961f8a9f4e091d5e48b6fe23910d09
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-26 01:41:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return "#{array[0]}"
   elsif array.size == 2 
      return array.join(" and ")
-   elsif array.length >= 3
+   elsif array.size >= 3
     array[-1] = "and #{array[-1]}"
     return array.join(", ")
   end
