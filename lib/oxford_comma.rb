3626f1842602a622dd497cbd3c17fcd3a7b0009e
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-26 01:36:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
-end
+  if array.size == 1
+    return "#{array[0]}"
+  elsif array.size == 2 
+     return array.join(" and ")
+   elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
+end 
